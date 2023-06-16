d7996df35bf4a207788ac67b5f99de3e5f2c8290
Tristan Sean Paul Perfecto Cinco <71287768+kolehiyolo@users.noreply.github.com>
2023-06-16 11:43:03 +0800

    Created Starter Files
    1. Created files for the listed components

--- src/index.js
+++ src/index.js
@@ -26,4 +26,4 @@ root.render(
 //6. Make sure that the final website is styled like the example shown here:
 //https://l1pp6.csb.app/
 
-//HINT: You will need to study the classes in teh styles.css file to appy styling.
+//HINT: You will need to study the classes in teh styles.css file to apply styling.
