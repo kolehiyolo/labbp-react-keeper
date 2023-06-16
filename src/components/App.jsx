f886fad3808d94a9bdfd22c3e4edc295effbb07d
Tristan Sean Paul Perfecto Cinco <71287768+kolehiyolo@users.noreply.github.com>
2023-06-16 12:00:53 +0800

    Added ".jsx" to Imports
    1. Angela mentioned that this is the convention AirBnB uses, which I want to follow moving forward

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import Header from './Header';
-import Footer from './Footer';
-import Note from './Note';
+import Header from './Header.jsx';
+import Footer from './Footer.jsx';
+import Note from './Note.jsx';
 
 function App() {
     return (<div>
