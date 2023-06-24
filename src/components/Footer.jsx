fb80f975dac7c9256fa48b208f9b1229d67a5b71
Tristan Sean Paul Perfecto Cinco <71287768+kolehiyolo@users.noreply.github.com>
2023-06-24 17:47:54 +0800

    Starting files ready
    1. Lesgawww

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from "react";
 
 function Footer() {
-    const currentYear = new Date().getFullYear();
-
-    return (<footer>
-            <p>Copyright ⓒ {currentYear}</p>
-        </footer>);
+  const year = new Date().getFullYear();
+  return (
+    <footer>
+      <p>Copyright ⓒ {year}</p>
+    </footer>
+  );
 }
 
 export default Footer;
