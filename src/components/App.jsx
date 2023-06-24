4654b2724a8f75e387a7cffbbeb60225384f70ca
Tristan Sean Paul Perfecto Cinco <71287768+kolehiyolo@users.noreply.github.com>
2023-06-24 17:49:14 +0800

    Generating list items through mapping
    1. Created a temporary pseudo-array of notesList to make sure generateListItem works
    2. Notes are now being dynamically generated based on the notesList state array

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+  const sampleNotesList = [
+    {
+      title: "Note 1",
+      content: "This is content"
+    },
+    {
+      title: "Note 2",
+      content: "This is content, too"
+    },
+  ];
+  const [notesList, setNotesList] = useState(sampleNotesList);
+
+  function generateListItem(item, index) {
+    return <Note key={index} title={item.title} content={item.content} />
+  }
+
   return (
     <div>
       <Header />
       <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
+      {notesList.map(generateListItem)}
       <Footer />
     </div>
   );
