156998fa76332cb8675f36d74c3b486ea8a62df1
Tristan Sean Paul Perfecto Cinco <71287768+kolehiyolo@users.noreply.github.com>
2023-06-24 17:55:46 +0800

    Input values being stored on text change
    1. CreateArea.jsx now takes the input elements' values whenever change is observed for each
    2. These values update an object state containing title and content properties
    3. Tested and confirmed to work via React Dev Tools

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 
 function CreateArea() {
+  const [note, setNote] = useState({
+    title: "",
+    content: ""
+  });
+
+  function onTextChange(event) {
+    const {name, value} = event.target;
+
+    setNote(prevValue => {
+      return {...prevValue, [name]: value};
+    });
+  }
+
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
+        <input
+          onChange={onTextChange}
+          name="title"
+          placeholder="Title"
+        />
+        <textarea
+          onChange={onTextChange}
+          name="content"
+          placeholder="Take a note..."
+          rows="3"
+        />
         <button>Add</button>
       </form>
     </div>
